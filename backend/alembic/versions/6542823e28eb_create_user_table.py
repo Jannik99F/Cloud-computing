f63b38f54f3278b3874e4eb68c0598775e57e5ac
timfinmans <tim@finmans.de>
2025-01-09 20:27:50 +0100

    The backend is now fully configured. The ORM can be used for writing and reading data from and to the database. The db management "tool" alembic was added which will take care of migrating the database.
    The database container is now also configured and can out of the box communicate with the webshop container.

---
+++ backend/alembic/versions/6542823e28eb_create_user_table.py
@@ -0,0 +1,35 @@
+"""create user table
+
+Revision ID: 6542823e28eb
+Revises: 
+Create Date: 2025-01-09 18:12:04.340999
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6542823e28eb'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.create_table(
+        'user',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('first_name', sa.String(50), nullable=False),
+        sa.Column('last_name', sa.String(50), nullable=False),
+        sa.Column('email', sa.String(50), nullable=False),
+        sa.Column('password', sa.String(50), nullable=False),
+        sa.Column('address', sa.String(50), nullable=False),
+    )
+    pass
+
+def downgrade():
+    op.drop_table('user')
+    pass
--- backend/app/hello.py
+++ backend/app/hello.py
@@ -1,14 +1,35 @@
-from typing import Union
-
 from fastapi import FastAPI
+from sqlmodel import Session, Field, SQLModel, create_engine, select
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = database_url = os.getenv("DB_URL_LOCAL") if os.getenv("DEBUG") == "true" else os.getenv("DB_URL_PROD")
+engine = create_engine(DATABASE_URL)
+
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+    address: str
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    SQLModel.metadata.create_all(engine)
 
+    with Session(engine) as session:
+        statement = select(User).where(User.first_name == "Tim")
+        user = session.exec(statement).first()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+        if(not user):
+            user = User(id=1, first_name="Tim", last_name="Finmans", email="tim@finmans.de", password="tim123", address="123 Main Street")
+            session.add(user)
+            session.commit()
+
+    return {"user": user}
