ba8bff393e9610a895fdf9333f0ffcfb35216af6
timfinmans <tim@finmans.de>
2025-01-27 11:05:35 +0100

    Found an error leading to the foreign for product_id in variance table to not be created.

--- backend/app/models/variance.py
+++ backend/app/models/variance.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel
 
 class Variance(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    product_id: int
+    product_id: int = Field(foreign_key="product.id")
     name: str
     variance_type: str
     price: float
