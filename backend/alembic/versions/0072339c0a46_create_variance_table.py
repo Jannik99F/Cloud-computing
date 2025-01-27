354dace28e391663940fc017ca9d81608d0e252f
timfinmans <tim@finmans.de>
2025-01-27 11:05:35 +0100

    Found an error leading to the foreign for product_id in variance table to not be created.

--- backend/alembic/versions/0072339c0a46_create_variance_table.py
+++ backend/alembic/versions/0072339c0a46_create_variance_table.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '0072339c0a46'
-down_revision: Union[str, None] = None
+down_revision: str = '93dd105f671a'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -22,18 +22,15 @@ def upgrade():
     op.create_table(
         'variance',
         sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('product_id', sa.Integer),
+        sa.Column(
+            'product_id',
+            sa.Integer,
+            sa.ForeignKey('product.id', ondelete='CASCADE'),  # Explicit foreign key
+            nullable=False
+        ),
         sa.Column('price', sa.Float, nullable=False),
         sa.Column('name', sa.String(50), nullable=False),
         sa.Column('variance_type', sa.String(50), nullable=False),
-    )
-
-    op.create_foreign_key(
-        constraint_name="fk_product_variance",
-        source_table="variance",
-        referent_table="product",
-        local_cols=["product_id"],
-        remote_cols=["id"]
     )
     pass
 
