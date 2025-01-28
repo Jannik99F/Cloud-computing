8cc8c78965596f9a8e723f570fb132a79fb3db75
timfinmans <tim@finmans.de>
2025-01-28 14:10:03 +0100

    The variance router no is related to the product via the urls. Furthermore, there were checks implemented when something is done about a products variance and the product doesn't exist.

--- backend/app/routers/variance_router.py
+++ backend/app/routers/variance_router.py
@@ -1,20 +1,21 @@
 from sqlmodel import Session, SQLModel, select
 from models.variance import Variance
 from models.product import Product
-from sqlalchemy.orm import selectinload
 
 from db.engine import DatabaseManager, get_session
 
 from fastapi import APIRouter, Request, HTTPException, Depends
 
 router = APIRouter(
-    prefix="/variances",
+    prefix="/products/{product_id}/variances",
     tags=["variances"]
 )
 
 @router.get("/")
-def get_all_variances(session: Session = Depends(get_session)):
-    statement = select(Variance)
+def get_all_variances(product_id, session: Session = Depends(get_session)):
+    check_product_exists(product_id)
+
+    statement = select(Variance).where(Variance.product_id == product_id)
     variances = session.exec(statement).all()
 
     variances = [variance.load_relations(relations_to_load=["product"]) for variance in variances]
@@ -22,7 +23,9 @@ def get_all_variances(session: Session = Depends(get_session)):
     return variances
 
 @router.get("/{variance_id}")
-def get_variance_by_id(variance_id: int, session: Session = Depends(get_session)):
+def get_variance_by_id(product_id: int, variance_id: int, session: Session = Depends(get_session)):
+    check_product_exists(product_id)
+
     statement = select(Variance).where(Variance.id == variance_id)
     variance = session.exec(statement).first()
 
@@ -32,10 +35,11 @@ def get_variance_by_id(variance_id: int, session: Session = Depends(get_session)
     return variance.load_relations(relations_to_load=["product"])
 
 @router.post("/")
-async def create_variance(request: Request, session: Session = Depends(get_session)):
+async def create_variance(product_id: int, request: Request, session: Session = Depends(get_session)):
+    check_product_exists(product_id)
+
     user_data = await request.json()
 
-    product_id = user_data.get("product_id")
     name = user_data.get("name")
     variance_type = user_data.get("variance_type")
     price = user_data.get("price")
@@ -59,7 +63,9 @@ async def create_variance(request: Request, session: Session = Depends(get_sessi
     return new_variance.load_relations(relations_to_load=["product"])
 
 @router.patch("/{variance_id}")
-async def update_variance(variance_id: int, request: Request, session: Session = Depends(get_session)):
+async def update_variance(product_id: int, variance_id: int, request: Request, session: Session = Depends(get_session)):
+    check_product_exists(product_id)
+
     data = await request.json()
 
     statement = select(Variance).where(Variance.id == variance_id)
@@ -67,9 +73,6 @@ async def update_variance(variance_id: int, request: Request, session: Session =
 
     if variance is None:
         raise HTTPException(status_code=404, detail="Variance not found.")
-
-    if "product_id" in data:
-        raise HTTPException(status_code=400, detail="product_id cannot be updated.")
 
     for key, value in data.items():
         if value is not None and hasattr(variance, key):
@@ -85,7 +88,9 @@ async def update_variance(variance_id: int, request: Request, session: Session =
 
 
 @router.delete("/{variance_id}")
-def delete_user(variance_id: int, session: Session = Depends(get_session)):
+def delete_user(product_id: int, variance_id: int, session: Session = Depends(get_session)):
+    check_product_exists(product_id)
+
     variance = session.get(Variance, variance_id)
 
     if variance is None:
@@ -95,3 +100,10 @@ def delete_user(variance_id: int, session: Session = Depends(get_session)):
     session.commit()
 
     return {"message": "Variance deleted successfully."}
+
+def check_product_exists(product_id: int):
+    statement = select(Product).where(Product.id == product_id)
+    product = get_session().exec(statement).first()
+
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found.")
