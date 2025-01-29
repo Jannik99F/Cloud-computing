8ac04d49d098c98904bc4455f38f443fa1f29dc2
timfinmans <tim@finmans.de>
2025-01-29 16:25:05 +0100

    The beginning for the user model was done and its router. Functions had to be added to the user model to avoid redundant code. Addiotionally, the model, migration and a part of the router for the order was implemented. The base_model.py relation fetching was adjusted to now also support nested relations which is necessary to be able to return a full order.

---
+++ backend/alembic/versions/3d0481fff148_add_order_table.py
@@ -0,0 +1,41 @@
+"""add order table
+
+Revision ID: 3d0481fff148
+Revises: 19f1e3d1fd11
+Create Date: 2025-01-29 14:06:26.891352
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '3d0481fff148'
+down_revision: Union[str, None] = '19f1e3d1fd11'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.create_table(
+        'order',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('shipping_address', sa.String(), nullable=True),
+        sa.Column('billing_address', sa.String(), nullable=True),
+        sa.Column('payment_method', sa.String(), nullable=True),
+        sa.Column('payed', sa.Boolean(), nullable=False),
+        sa.Column('status', sa.String(), nullable=False),
+        sa.Column(
+            'basket_id',
+            sa.Integer,
+            sa.ForeignKey('basket.id', ondelete='CASCADE'),
+            nullable=False
+        ),
+    )
+    pass
+
+def downgrade():
+    op.drop_table('order')
+    pass
