ed0266c65b9f57f15ae628c064730ab42be8090e
timfinmans <tim@finmans.de>
2025-01-29 12:54:42 +0100

    The functions and routes were implemented which make it possible to add and remove items. Also, the amount of a item existing in the basket can be increased and decreased. The decision was made to not allow the amount to go beneath zero, what of course makes sense. The route decreasing the amount isn't able to delete a basket item to keep up REST standards and will just return a 400 telling the amount would be below zero.

--- backend/app/models/basket_item.py
+++ backend/app/models/basket_item.py
@@ -1,12 +1,13 @@
 from sqlmodel import Field, SQLModel, Relationship
 from models.base_model import BaseModel
+from typing import Optional
 
 class BasketItem(BaseModel, table=True):
     id: int = Field(default=None, primary_key=True)
     variance_id: int = Field(foreign_key="variance.id")
     basket_id: int = Field(foreign_key="basket.id")
     amount: int
-    base_price: float
-    variance_price: float
+    base_price: Optional[float] = Field(default=None, nullable=True)
+    variance_price: Optional[float] = Field(default=None, nullable=True)
     basket: "Basket" = Relationship(back_populates="basket_items")
     variance: "Variance" = Relationship(back_populates="basket_items")
