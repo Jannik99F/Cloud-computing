defb9caea2f7cacda232ea5e9b968a48cb335617
Lmar Niazman <niazman@stud.fra-uas.de>
2025-02-25 16:08:23 +0100

    Cleanup and progressed item amount

--- frontend/vue-project/src/views/ProductDetails.vue
+++ frontend/vue-project/src/views/ProductDetails.vue
@@ -1,33 +1,38 @@
-<template>
-    <!-- If product couldn't be retrieved -->
-    <div v-if="(product instanceof Error)">
-        <p>{{ product.message }}</p>
-        <p>Test</p>
-    </div>
-
-    <div v-else-if="product && !(product instanceof Error) 
-                 && variances && !(variances instanceof Error)
-                 && inventory && !(inventory instanceof Error)" 
-                 class="product-box">
-        <h1 class="product-header">{{ product.name }}</h1>
-        <p>{{ product.product_type }}</p>
-        <p>{{ product.height }}x{{ product.width }}x{{ product.depth }}cm</p>
-        <div id="product-image">
-            <!-- 
-                TODO
-                Put product images between product details and variances
-            -->
+<template v-if="ready">
+    <div id="product-details" v-if="ready">
+        <!-- If product couldn't be retrieved -->
+        <div v-if="(product instanceof Error)">
+            <p>{{ product.message }}</p>
+            <p>Test</p>
         </div>
-        <div class="variance-box">
-            <div v-for="variance in variances" :key="variance.id" class="variance-item">
-                <br>
-                <div :style="{ backgroundColor: variance.name }"></div>
-                <p>{{ variance.name }}</p>
-                <p>{{ formatPrice(variance.price*product.base_price) }}</p>
-                <p>Amount available: {{ getAmount(variance.id) }}</p>
+
+        <div v-else-if="product && !(product instanceof Error) 
+                    && variances && !(variances instanceof Error)
+                    && inventory && !(inventory instanceof Error)" 
+                    class="product-box">
+            <h1 class="product-header">{{ product.name }}</h1>
+            <p>{{ product.product_type }}</p>
+            <p>{{ product.height }}x{{ product.width }}x{{ product.depth }}cm</p>
+            <div id="product-image">
+                <!-- 
+                    TODO
+                    Put product images between product details and variances
+                -->
+            </div>
+            <div class="variance-box">
+                <div v-for="variance in variances" :key="variance.id" class="variance-item">
+                    <p>{{ JSON.stringify(variance) }}</p>
+                    <br>
+                    <div :style="{ backgroundColor: variance.name }"></div>
+                    <p>{{ variance.name }}</p>
+                    <p>{{ formatPrice(variance.price*product.base_price) }}</p>
+                    <p>Amount available: {{ getAmount(variance.id) }}</p>
+                </div>
             </div>
         </div>
     </div>
+
+    <p v-else>Loading...</p>
 </template>
 
 <script setup lang="ts">
@@ -36,29 +41,49 @@
     import { formatPrice } from '@/models/Product.vue';
     import type { Variance } from '@/models/Variance.vue';
     import type { Inventory } from '@/models/Inventory.vue';
-    import { onMounted, ref } from 'vue';
+    import { onMounted, ref, toRaw } from 'vue';
     import { useRoute } from 'vue-router';
 
-    const product = ref<Product | Error>()
-    const variances = ref<Variance[] | Error>()
-    const inventory = ref<Inventory[] | Error>()
+    const ready = ref(false) // Only show template when ready is true
 
-    onMounted(() => {
+    const product = ref<Product>()
+    const variances = ref<Variance[]>()
+    const inventory = ref<Inventory[]>()
+
+    onMounted(async () => {
         console.log('Product details page mounted')
 
         const route = useRoute()
-        getProduct(route.params.id.toString()) // Init the product ref
-        getVariances(route.params.id.toString()) // Init the variances ref
-        getInventory() // Init the inventory ref
+
+        await getProduct(route.params.id.toString()) // Init the product ref
+        await getVariances(route.params.id.toString()) // Init the variances ref
+        await getInventory() // Init the inventory ref
+
+        ready.value = true;
     })
 
     const getAmount = (variance_id: number) => {
-        if(inventory.value instanceof Error || inventory.value == undefined) return 0;
-        for(const item of inventory.value) {
-            if(item.variance_id == variance_id) {
-                return item.amount
-            }
+        console.log("Getting inventory for variance with id ", variance_id)
+
+        if(inventory.value == undefined) {
+            console.log("inventory undefined")
+            return 0;
         }
+
+        for(const item of inventory.value) {
+            console.log("Checking item: ", item)
+            const rawItem = toRaw(item)
+            console.log("Raw item: ", rawItem)
+            console.log("Item id: ", rawItem.id)
+            if(item.variance_id == variance_id) {
+                return rawItem.amount
+            }
+            return 0;
+        }
+
+        inventory.value.forEach((item) => {
+            
+        })
     }
 
     /**
@@ -71,30 +96,37 @@
             product.value = await response.json()
         } catch (error) {
             console.error('Error fetching product with id ' + id, error)
-            product.value = new Error('Error fetching product')
         }
     }
 
-    const getVariances = async(id: string) => {
+    const getVariances = async (id: string) => {
         try {
             const response = await fetch(`http://localhost:8000/products/${id}/variances`)
             variances.value = await response.json()
         } catch (error) {
             console.error('Error fetching variances for product with id ' + id, error)
-            variances.value = new Error('Error fetching variances')
         }
     }
 
-    const getInventory = async() => {
-        if(variances.value instanceof Error || variances.value == undefined)
-            console.log(variances.value);
-        try {
-            const response = await fetch(`http://localhost:8000/inventory/${variance_id}`)
-            inventory.value = await response.json()
-        } catch (error) {
-            console.error('Error fetching inventory for variance with id ' + variance_id, error)
-            inventory.value = new Error('Error fetching inventory')
+    const getInventory = async () => {
+        let jsonResponses = [];
+
+        if(variances.value == undefined) {
+            console.log("Variances are not defined");
+            return
         }
+
+        for(const variance of variances.value) {
+            try {
+                const response = await fetch(`http://localhost:8000/inventory/${variance.id}`)
+                jsonResponses.push(await response.json())
+            } catch (error) {
+                console.error('Error fetching inventory for variance with id ' + variance.id, error)
+            }
+        }
+
+        inventory.value = jsonResponses;
+        console.log("Inventory: ", inventory.value)
     }
 </script>
 
